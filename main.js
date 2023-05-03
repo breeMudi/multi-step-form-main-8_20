f7a44f0ebcba2663c141d2b59c89bbb2379dbca4
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-05-03 20:20:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const steps = document.querySelector(".step")
-// const stp_btn =steps.getAttribute('button')
+const innerContent = document.querySelectorAll('.inner-content')
+const nextBtn = document.querySelector('.next')
+const sectionList = ['section1', 'section2m', 'section3m', 'section4m', 'section2y', 'section3y', 'section4y', 'success']
+console.log(innerContent)
+const random = [34, 64, 77]
+console.log(innerContent[2])
 
-steps.addEventListener('click', (e) => {
-    if(e.target.className === 'step' ){
-        alert('i have been clicked')
-    }
-    // alert('i have been clicked')
+
+// initialize forms
+innerContent[0].style.display = 'block'
+innerContent[1,2,3,4,5,6,7].style.display = 'none'
+
+// add event listener for 
+nextBtn.addEventListener('click', (e) => {
+    const eId = e.target.parentElement.parentElement.id
+    console.log(eId)
+    console.log(sectionList.indexOf(eId) + 1)
 })
+
+
+
