ebbb0838b39c83f7b8ea0a68675b39bda719ace9
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-05-04 23:10:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const innerContent = document.querySelectorAll('.inner-content')
 const nextBtn = document.querySelector('.next')
 const sectionList = ['section1', 'section2m', 'section3m', 'section4m', 'section2y', 'section3y', 'section4y', 'success']
-// const switchBtn = document.querySelector('.switch-btn')
+
+
+// save all clicked of a particular type in form into variables
+// form class === form-input
+
+function reset () {
+    document.querySelector('.error').textContent = ''
+}
+
+// create a function that checks if input is empty
+function inputCheck () {
+    reset()
+    if(document.querySelector('#username').value === ''){
+        document.querySelector('#error1').textContent = 'This field is required'}
+    if(document.querySelector('#email').value === ''){
+        document.querySelector('#error2').textContent = 'This field is required'}
+    if(document.querySelector('#phone').value === ''){
+        document.querySelector('#error3').textContent = 'This field is required'}
+}
+
+
 
 const section2m = document.querySelector('#section2m')
 const section2y = document.querySelector('#section2y')
-// const switchBtn = document.querySelector('.switch-btn')
 
 section2m.addEventListener('click', (e) => {
     const clicked = e.target.className
-    if(clicked === 'switch-btn' || clicked === 'switch=click'){
+    if(clicked === 'switch-btn' || clicked === 'switch-click'){
         innerContent[1].style.display = 'none'
         innerContent[4].style.display = 'block'
     }
@@ -17,19 +36,22 @@ section2m.addEventListener('click', (e) => {
 
 section2y.addEventListener('click', (e) => {
     const clicked = e.target.className
-    if(clicked === 'switch-btn' || clicked === 'switch=click'){
+    if(clicked === 'switch-btn' || clicked === 'switch-click'){
         innerContent[4].style.display = 'none'
         innerContent[1].style.display = 'block'
     }
 })
 
+// next and back button
 addEventListener('click', (e) => {
     console.log(e.target.className)
     if(e.target.className === 'next'){
         const eId = e.target.parentElement.parentElement.id
         const currentId = sectionList.indexOf(eId)
+        inputCheck()
         innerContent[currentId].style.display = 'none'
         innerContent[currentId + 1].style.display = 'block'
+        
     }
     if(e.target.className === 'back'){
         const e2Id = e.target.parentElement.parentElement.id
@@ -40,5 +62,3 @@ addEventListener('click', (e) => {
 })
 
 // change step bg color
-
-// save all clicked of a particular type into variables
