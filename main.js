b2ca288469850ee51c360d3e6fa9bf372221c20f
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-05-04 20:13:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 const innerContent = document.querySelectorAll('.inner-content')
 const nextBtn = document.querySelector('.next')
 const sectionList = ['section1', 'section2m', 'section3m', 'section4m', 'section2y', 'section3y', 'section4y', 'success']
-console.log(innerContent)
-const random = [34, 64, 77]
-console.log(innerContent[2])
+// const switchBtn = document.querySelector('.switch-btn')
 
+const section2m = document.querySelector('#section2m')
+const section2y = document.querySelector('#section2y')
+// const switchBtn = document.querySelector('.switch-btn')
 
-// initialize forms
-// innerContent[0].style.display = 'block'
-// innerContent[1,2,3,4,5,6,7].style.display = 'none'
+section2m.addEventListener('click', (e) => {
+    const clicked = e.target.className
+    if(clicked === 'switch-btn' || clicked === 'switch=click'){
+        innerContent[1].style.display = 'none'
+        innerContent[4].style.display = 'block'
+    }
+})
 
-// add event listener for 
-// nextBtn.addEventListener('click', (e) => {
-//     const eId = e.target.parentElement.parentElement.id
-//     console.log(eId)
-//     const currentId = sectionList.indexOf(eId)
-//     console.log(currentId)
-//     innerContent[currentId].style.display = 'none'
-//     innerContent[currentId + 1].style.display = 'block'
-// })
+section2y.addEventListener('click', (e) => {
+    const clicked = e.target.className
+    if(clicked === 'switch-btn' || clicked === 'switch=click'){
+        innerContent[4].style.display = 'none'
+        innerContent[1].style.display = 'block'
+    }
+})
 
 addEventListener('click', (e) => {
     console.log(e.target.className)
-    if(e.target.className = 'next'){
+    if(e.target.className === 'next'){
         const eId = e.target.parentElement.parentElement.id
-        console.log(eId)
         const currentId = sectionList.indexOf(eId)
-        console.log(currentId)
         innerContent[currentId].style.display = 'none'
         innerContent[currentId + 1].style.display = 'block'
     }
+    if(e.target.className === 'back'){
+        const e2Id = e.target.parentElement.parentElement.id
+        const currentId = sectionList.indexOf(e2Id)
+        innerContent[currentId].style.display = 'none'
+        innerContent[currentId - 1].style.display = 'block'
+    }
 })
+
+// change step bg color
+
+// save all clicked of a particular type into variables
